4bcb40bed0f71bd37379859287e995c3424da39a
白玉虎 <13935780227@163.com>
2026-01-25 13:02:30 +0800

    fix: null

--- baiyi-contract/src/base/06VariableEnum.sol
+++ baiyi-contract/src/base/06VariableEnum.sol
@@ -1,4 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract TestVariable {}
+// 定义一个枚举类型：enum 枚举名 { 成员 }
+// 枚举成员从 0 编号、不能多于 256 个成员
+contract TestEnum {
+    enum Color {
+        Red,
+        White,
+        Blue
+    }
+    Color color;
+
+    function setColor(Color c) public {
+        color = c;
+    }
+
+    function getMaxValue() public pure returns (Color) {
+        return type(Color).max;
+    }
+
+    function getMinValue() public pure returns (Color) {
+        return type(Color).min;
+    }
+}
