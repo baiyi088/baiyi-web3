acf0d6365950de40487b14775a547fade70d4d0c
白玉虎 <13935780227@163.com>
2026-01-25 22:55:04 +0800

    feat: 练习题的尝试

--- baiyi-contract/src/04lianxi/01Bank_V1.sol
+++ baiyi-contract/src/04lianxi/01Bank_V1.sol
@@ -6,4 +6,35 @@
 // 练习题
 // https://decert.me/quests/c43324bc-0220-4e81-b533-668fa644c1c3
 
+contract Bank {
+    mapping(address => uint256) public balances;
+    address[] public top3;
+    address public admin;
 
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+        if (top3.length == 0) {
+            top3.push(msg.sender);
+        } else {
+            for (uint256 i = 0; i < top3.length; i++) {
+                if (balances[msg.sender] > balances[top3[i]]) {
+                    // top3.insert(i, msg.sender);
+                    if (top3.length > 3) {
+                        top3.pop();
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    function withdraw() public {
+        require(msg.sender == admin, "Only admin can withdraw");
+        uint256 amount = address(this).balance;
+        payable(admin).transfer(amount);
+    }
+}
